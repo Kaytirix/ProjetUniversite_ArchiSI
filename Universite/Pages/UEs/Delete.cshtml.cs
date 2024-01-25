984d725d8cb7b9f3d9e4f240b4715e9c5ed9f821
Kaytirix <63360693+Kaytirix@users.noreply.github.com>
2024-01-25 18:29:15 +0100

    Ajout pages CRUD UE

---
+++ Universite/Pages/UEs/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Universite.Data;
+using Universite.Model;
+
+namespace Universite.Pages.UEs
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Universite.Data.ApplicationDbContext _context;
+
+        public DeleteModel(Universite.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public UE UE { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ue = await _context.UE.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (ue == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                UE = ue;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ue = await _context.UE.FindAsync(id);
+            if (ue != null)
+            {
+                UE = ue;
+                _context.UE.Remove(UE);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Universite/Pages/UEs/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Universite.Data;
+using Universite.Model;
+
+namespace Universite.Pages.UEs
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Universite.Data.ApplicationDbContext _context;
+
+        public DetailsModel(Universite.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public UE UE { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ue = await _context.UE.FirstOrDefaultAsync(m => m.ID == id);
+            if (ue == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                UE = ue;
+            }
+            return Page();
+        }
+    }
+}
